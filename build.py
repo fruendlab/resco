7bcdbd26d32b8d6fcb8d8a8e7b40dd144fd5a4d3
Ingo Fruend <mail@ingofruend.net>
2017-10-23 14:18:51 -0400

    initial commit

---
+++ build.py
@@ -0,0 +1,19 @@
+from pybuilder.core import use_plugin, init
+from pybuilder.vcs import count_travis
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+
+
+name = "resco"
+default_task = "publish"
+version = count_travis()
+
+
+@init
+def set_properties(project):
+    project.depend_on('fabric3')
