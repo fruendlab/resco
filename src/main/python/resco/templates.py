91c126f8c447c68fc314bca32cd7449845499483
Ingo Fruend <mail@ingofruend.net>
2017-10-27 19:00:12 -0400

    updateing template

--- src/main/python/resco/templates.py
+++ src/main/python/resco/templates.py
@@ -9,14 +9,20 @@ from fabric.api import local
 from resco import api
 
 remote_working_dir = ''
-venv = api.RemoteVirtualEnv({{ module }}-env,
+venv = api.RemoteVirtualEnv('{{ module }}-env',
                             dependencies=['requirements/all.txt',
                                           'requirements/remote.txt'])
 
 run_unit_tests = api.run_unit_tests
 
+
 def run_script(script_name):
     api.run_script(script_name, venv, '{{ module }}', remote_working_dir)
+
+
+def update_venv():
+    venv.install()
+
 
 def install_local():
     local('pip install -r requirements/all.txt -r requirements/local.txt')
