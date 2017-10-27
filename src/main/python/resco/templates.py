4ab2decc1d9d80b4dec8b6fb5890cb6664e04591
Ingo Fruend <mail@ingofruend.net>
2017-10-27 18:16:16 -0400

    adding api functions

--- src/main/python/resco/templates.py
+++ src/main/python/resco/templates.py
@@ -5,4 +5,14 @@ This is an empty readme.
 """
 
 fabfile = """
+from resco api
+
+remote_working_dir = ''
+venv = api.RemoteVirtualEnv({{ module }}-env,
+                            dependencies=[])
+
+run_unit_tests = api.run_unit_tests
+
+def run_script(script_name):
+    api.run_script(script_name, venv, '{{ module }}', remote_working_dir)
 """
---
+++ src/unittest/python/api_tests.py
@@ -0,0 +1,80 @@
+from unittest import TestCase, mock
+
+from resco import api
+
+
+class ContextVerifier(object):
+
+    def __enter__(self, *args, **kwargs):
+        self.calls = []
+        self.active = True
+
+    def __exit__(self, *args):
+        self.active = False
+
+    def add_call(self, *args, **kwargs):
+        if self.active:
+            self.calls.append((args, kwargs))
+
+    def attach(self, context, target):
+        context.return_value = self
+        target.side_effect = self.add_call
+
+
+class TestRunScript(TestCase):
+
+    def setUp(self):
+        self.patches = mock.patch.multiple('resco.api',
+                                           local=mock.DEFAULT,
+                                           put=mock.DEFAULT,
+                                           run=mock.DEFAULT,
+                                           cd=mock.DEFAULT,
+                                           )
+        self.mocks = self.patches.start()
+        self.mock_venv = mock.MagicMock()
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_copies_files(self):
+        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        self.mocks['put'].assert_any_call('ANY_MODULE', '')
+
+    def test_copies_scripts(self):
+        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        self.mocks['put'].assert_any_call('scripts', '')
+
+    def test_supports_working_dir(self):
+
+        cv = ContextVerifier()
+        cv.attach(self.mocks['cd'], self.mocks['put'])
+
+        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+
+        self.mocks['cd'].assert_called_once_with('ANY_DIR')
+        self.assertEqual(len(cv.calls), 2)
+
+    def test_runs_remotely(self):
+        cv = ContextVerifier()
+        cv.attach(self.mocks['cd'], self.mocks['run'])
+        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        self.mocks['run'].assert_called_once_with('python scripts/ANY_SCRIPT')
+        self.assertEqual(len(cv.calls), 1)
+
+    def test_works_with_prefix(self):
+        api.run_script('scripts/ANY_SCRIPT',
+                       self.mock_venv,
+                       'ANY_MODULE',
+                       'ANY_DIR')
+        self.mocks['run'].assert_called_once_with('python scripts/ANY_SCRIPT')
+
+    @mock.patch('resco.api.run_unit_tests')
+    def test_runs_unit_tests_first(self, mock_run_unit_tests):
+        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        mock_run_unit_tests.assert_called_once_with()
+
+    def test_runs_in_venv(self):
+        cv = ContextVerifier()
+        cv.attach(self.mock_venv.activate, self.mocks['run'])
+        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        self.assertEqual(len(cv.calls), 1)
