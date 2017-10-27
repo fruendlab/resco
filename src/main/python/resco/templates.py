db510ba8f45c5f7cac7d97fa28b31606010952f9
Ingo Fruend <mail@ingofruend.net>
2017-10-27 18:23:13 -0400

    requirements added

--- src/main/python/resco/templates.py
+++ src/main/python/resco/templates.py
@@ -5,14 +5,19 @@ This is an empty readme.
 """
 
 fabfile = """
+from fabric.api import local
 from resco api
 
 remote_working_dir = ''
 venv = api.RemoteVirtualEnv({{ module }}-env,
-                            dependencies=[])
+                            dependencies=['requirements/all.txt',
+                                          'requirements/remote.txt'])
 
 run_unit_tests = api.run_unit_tests
 
 def run_script(script_name):
     api.run_script(script_name, venv, '{{ module }}', remote_working_dir)
+
+def install_local():
+    local('pip install -r requirements/all.txt -r requirements/local.txt')
 """
