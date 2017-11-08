9884af68a6ff4edfa52c06ed460c3141b52a29e9
Ingo Fruend <mail@ingofruend.net>
2017-11-08 18:23:24 -0500

    start script

--- src/unittest/python/api_tests.py
+++ src/unittest/python/api_tests.py
@@ -21,7 +21,7 @@ class ContextVerifier(object):
         target.side_effect = self.add_call
 
 
-class TestRunScript(TestCase):
+class TestRunCommand(TestCase):
 
     def setUp(self):
         self.patches = mock.patch.multiple('resco.api',
@@ -37,34 +37,87 @@ class TestRunScript(TestCase):
         mock.patch.stopall()
 
     def test_copies_files(self):
-        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        api.run_command('{cmd}:{module}',
+                        'ANY_SCRIPT',
+                        self.mock_venv,
+                        'ANY_MODULE',
+                        'ANY_DIR')
         self.mocks['put'].assert_any_call('ANY_MODULE', 'ANY_DIR')
 
     def test_copies_scripts(self):
-        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        api.run_command('{cmd}:{module}',
+                        'ANY_SCRIPT',
+                        self.mock_venv,
+                        'ANY_MODULE',
+                        'ANY_DIR')
         self.mocks['put'].assert_any_call('scripts', 'ANY_DIR')
 
     def test_runs_remotely(self):
         cv = ContextVerifier()
         cv.attach(self.mocks['cd'], self.mocks['run'])
-        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
-        self.mocks['run'].assert_called_once_with('python scripts/ANY_SCRIPT')
+        api.run_command('{cmd}:{module}',
+                        'scripts/ANY_SCRIPT',
+                        self.mock_venv,
+                        'ANY_MODULE',
+                        'ANY_DIR')
+        self.mocks['run'].assert_called_once_with(
+            'scripts/ANY_SCRIPT:ANY_MODULE')
         self.assertEqual(len(cv.calls), 1)
 
     def test_works_with_prefix(self):
-        api.run_script('scripts/ANY_SCRIPT',
-                       self.mock_venv,
-                       'ANY_MODULE',
-                       'ANY_DIR')
-        self.mocks['run'].assert_called_once_with('python scripts/ANY_SCRIPT')
+        api.run_command('{cmd}:{module}',
+                        'scripts/ANY_SCRIPT',
+                        self.mock_venv,
+                        'ANY_MODULE',
+                        'ANY_DIR')
+        self.mocks['run'].assert_called_once_with(
+            'scripts/ANY_SCRIPT:ANY_MODULE')
 
     @mock.patch('resco.api.run_unit_tests')
     def test_runs_unit_tests_first(self, mock_run_unit_tests):
-        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        api.run_command('{cmd}:{module}',
+                        'ANY_SCRIPT',
+                        self.mock_venv,
+                        'ANY_MODULE',
+                        'ANY_DIR')
         mock_run_unit_tests.assert_called_once_with()
 
     def test_runs_in_venv(self):
         cv = ContextVerifier()
         cv.attach(self.mock_venv.activate, self.mocks['run'])
-        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        api.run_command('{cmd}:{module}',
+                        'ANY_SCRIPT',
+                        self.mock_venv,
+                        'ANY_MODULE',
+                        'ANY_DIR')
         self.assertEqual(len(cv.calls), 1)
+
+
+class TestRunScriptStartScript(TestCase):
+
+    def setUp(self):
+        self.mock_run_command = mock.patch('resco.api.run_command').start()
+        self.mock_venv = mock.MagicMock()
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_run_script_calls_correctly(self):
+        api.run_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        self.mock_run_command.assert_called_once_with(
+            '{cmd}',
+            'python scripts/ANY_SCRIPT',
+            self.mock_venv,
+            'ANY_MODULE',
+            'ANY_DIR',
+        )
+
+    def test_start_script_calls_correctly(self):
+        api.start_script('ANY_SCRIPT', self.mock_venv, 'ANY_MODULE', 'ANY_DIR')
+        self.mock_run_command.assert_called_once_with(
+            'tmux new-session -d -s {module} "{cmd}"',
+            'python scripts/ANY_SCRIPT',
+            self.mock_venv,
+            'ANY_MODULE',
+            'ANY_DIR',
+        )
